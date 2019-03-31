76e262aef39a84e03b0178c33993dde7ca0eaef5
ming <ming.hentech@gmail.com>
2019-03-31 03:27:36 +0000

    Add cookie login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
   def new
-    puts "aaaa"
   end
   
   def create
-    user = User.find_by(login_code: params[:login_code].downcase)
-    if user && user.authenticate(params[:password])
+    user = User.find_by(login_code: params[:session][:login_code].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid code/password combination'
       render 'new'
     end
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
   
